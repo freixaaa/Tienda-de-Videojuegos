796616c877c3956ca2ef32a7157c32ab84fd95c0
freixaaa <fiorella07freixa23.r@gmail.com>
2026-04-15 01:13:39 -0600

    Create archivos.py

---
+++ utils/archivos.py
@@ -0,0 +1,29 @@
+import json
+import csv
+
+
+def cargar_json(ruta):
+    with open(ruta, "r") as archivo:
+        return json.load(archivo)
+
+
+def guardar_json(ruta, datos):
+    with open(ruta, "w") as archivo:
+        json.dump(datos, archivo, indent=4)
+
+
+def cargar_csv(ruta):
+    datos = []
+    with open(ruta, newline="") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            datos.append(fila)
+    return datos
+
+
+def guardar_csv(ruta, datos):
+    with open(ruta, "w", newline="") as archivo:
+        campos = datos[0].keys()
+        escritor = csv.DictWriter(archivo, fieldnames=campos)
+        escritor.writeheader()
+        escritor.writerows(datos)
